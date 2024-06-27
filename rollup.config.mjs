da9ecdc5417800ccb5d15d7ae04168af7372f5d1
esheyw <esheyw@gmail.com>
2024-06-27 16:34:06 -0700

    rename root code folder

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "scripts/init.mjs",
+  input: "src/init.mjs",
   output: {
     file: "mhl.mjs",
     format: "es",
